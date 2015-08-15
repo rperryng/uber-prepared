d8c185e6d1ac8176931996a8885e8f2514ad5da7
rperryng <rperry@getpebble.com>
2015-08-15 01:35:44 -0700

    Session management setup

--- web/server.js
+++ web/server.js
@@ -5,10 +5,24 @@ var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var logger = require('logger');
 var env = require('node-env-file');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+var mongoose = require('mongoose');
 
 env(__dirname + '/.env');
 
 var app = express();
+
+// Connect to MongoDB
+mongoose.connect(process.env.DB_URI);
+
+// Session management
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  store: new MongoStore({ mongooseConnection: mongoose.connection}),
+  resave: false,
+  saveUninitialized: false
+}));
 
 // Parse url encoded form data
 app.use(bodyParser.json());
@@ -22,6 +36,7 @@ app.use(morgan('dev', {stream: logger.morganStream}));
 app.post('/twilio-callback', function (req, res, next) {
   logger.debug('from: %s', req.body.From);
   logger.debug('with message %s', req.body.Body);
+
   res.sendStatus(200);
 });
 
